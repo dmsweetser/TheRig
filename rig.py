1da26ffdd3b00af2327bfb98ccd630da30df38a5
Daniel Sweetser <dmsweetser@gmail.com>
2024-12-26 08:05:26 -0500

    Working towards further consolidation

--- rig.py
+++ rig.py
@@ -10,27 +10,7 @@ import threading
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 import requests
-
-    # "core_url_cleanup": "http://192.168.56.1:5032/process_request_cleanup",
-    # "core_url_creative": "http://192.168.56.1:5032/process_request_creative",
-    # "max_file_size": 10485760,
-    # "model_folder": "models/",
-    # "revisions_per_page": 10,
-    # "default_prompt": "Generate ONLY a production-ready revision of this code surrounded by triple-backticks, without additional commentary, that resolves all execution errors, completely implements all existing features and adds 2 additional new features. The code you generate should be entirely self-contained, not relying on any external assets such as images or font files. Include code that verifies on start that every function in the code executes without error.",
-    # "model_filename_creative": "Mistral-Nemo-Instruct-2407-Q8_0.gguf",
-    # "model_filename_cleanup": "Mistral-Nemo-Instruct-2407-Q8_0.gguf",
-    # "model_url": "about:blank",
-    # "port": "5032",    
-    # "host": "0.0.0.0",
-    # "session_type": "filesystem",
-    # "secret_key": "your_secret_key",
-    # "extract_from_markdown": True,
-    # "revision_prompt": "Generate ONLY a production-ready revision of the code above surrounded by triple-backticks, without additional commentary, that resolves all execution errors, completely removes all comments, completely implements all features, and completely removes redundant code. The code you generate should be entirely self-contained, not relying on any external assets such as images or font files. Include code that verifies on start that every function in the code executes without error.",
-    # "log_folder": "logs/",
-    # "wrap_up_cutoff": 70000,
-    # "requirements_prompt": "Generate ONLY a sample requirements.txt surrounded by triple-backticks that would work for this code without version numbers or additional comments.",
-    # "nuget_prompt": "Generate ONLY a sample packages.config surrounded by triple-backticks that would work for this code without version numbers or additional comments.",
-    # "error_prompt": "Here is the latest execution error when I try to run the code:"
+import time
 
 # Load config
 class Config:
@@ -63,8 +43,33 @@ if config.LOGGING_ENABLED:
 else:
     logging.basicConfig(level=logging.INFO)
 
-# LLM function
-def run_llm(prompt, input_code, language):
+def extract_code_blocks(markdown_text, return_first_only):
+    code_blocks = []
+    lines = markdown_text.split('\\n')
+    in_code_block = False
+    code_block = []
+
+    for line in lines:
+        if line.startswith('```') and in_code_block == False:
+
+            in_code_block = True
+
+        elif line.startswith('```') and in_code_block == True:
+            if code_block:
+                code_blocks.append('\\n'.join(code_block))
+            in_code_block = False
+            code_block = []
+        elif in_code_block:
+            code_block.append(line)
+
+    if return_first_only and len(code_blocks) > 0:
+        return code_blocks[0]
+
+    return '\\n\\n'.join(code_blocks)
+
+def run_llm(original_code, llama_model, prompt, language):
+    start_time = time.time() # Get the start time
+
     try:
         if config.LLM_API == 'local':
             # Instantiate LLM for each iteration
@@ -96,7 +101,7 @@ def run_llm(prompt, input_code, language):
 
             # Generate complete revision of code, addressing build errors, surrounded by triple backticks
             response = llama.create_completion( f"{prompt}")
-            output_code = response['choices'][0]['text'].split("```")[1].split("```")[0]
+            revised_code = response['choices'][0]['text']
         else:
             # Use OpenAI-compatible API
             response = requests.post(
@@ -115,12 +120,52 @@ def run_llm(prompt, input_code, language):
                     "timeout": None
                 }
             )
-            output_code = response.json()['choices'][0]['text'].split("```")[1].split("```")[0]
-        return output_code
+            revised_code = response.json()['choices'][0]['text']
     except Exception as e:
         # Handle LLM call failure, return input code without throwing errors
         logging.error(f"LLM call failed: {e}")
-        return input_code
+        return original_code
+
+    # Get the current ticks as a string
+    now = datetime.datetime.now()
+    current_ticks = now.strftime("%Y%m%d_%H%M%S")
+
+    file_name = "raw_response_"
+
+    with open(f"{config.LOG_FOLDER}{file_name}{current_ticks}.json", "w") as outfile:
+        json.dump(revised_code, outfile, indent=4)
+
+    # Check if extracting from Markdown is enabled in config
+    extract_from_markdown = get_config('extract_from_markdown',False)
+
+    return_first_only = False
+    if config.REQUIREMENTS_PROMPT in prompt:
+        return_first_only = True
+    if config.NUGET_PROMPT in prompt:
+        return_first_only = True
+
+    if extract_from_markdown:
+        revised_code = extract_code_blocks(revised_code, return_first_only)
+
+    end_time = time.time() # Get the end time
+    duration = end_time - start_time # Calculate the duration
+
+    print(f"Total duration in seconds: {duration}")
+
+    logger.log(f"Original code length (not tokens): {len(original_code)}")
+    logger.log(f"New code length (not tokens): {len(revised_code)}")
+
+    if len(revised_code) < 0.8 * len(original_code) and len(original_code) > 10000:
+        logger.log(f"Generated code was too short")
+        return original_code
+    elif len(revised_code) == 0:
+        logger.log(f"Generated code was blank")
+        return original_code
+    elif len(revised_code) > 1.7 * len(original_code) and len(original_code) > 10000:
+        logger.log(f"Generated code was too long")
+        return original_code
+    else:
+        return revised_code
 
 # Docker execution function
 def execute_code(code, language):
@@ -167,12 +212,12 @@ def execute_code(code, language):
 
 # Function to sanitize app name
 def sanitize_app_name(app_name):
-    return re.sub(r'[^a-zA-Z0-9_\\-]', '', app_name)
+    return re.sub(r'[^a-zA-Z0-9_\-]', '', app_name)
 
 # Function to process app request
-def process_app_request(app_name, prompt, language, input_code):
+def process_app_request(app_name, prompt, language, input_code, logger):
     # Run LLM
-    output_code = run_llm(prompt, input_code, language)
+    output_code = run_llm(input_code, None, prompt, logger, language)
 
     # Execute code in Docker
     build_output = execute_code(output_code, language)
@@ -217,7 +262,7 @@ def main():
     app_requests = read_app_requests()
     while True:
         for app_name, prompt, language, input_code in app_requests:
-            build_output = process_app_request(app_name, prompt, language, input_code)
+            build_output = process_app_request(app_name, prompt, language, input_code, logger)
             logging.info(f"Processed {app_name} with build output: {build_output}")
 
             # Get default prompt from config or use a default value
@@ -239,7 +284,7 @@ def main():
                 message = f"<INST>{prompt}\\n</INST> </s>\\n"
 
             # Get response from LLM
-            output_code = run_llm(message, input_code, language)
+            output_code = run_llm(input_code, None, message, language)
 
             # Execute code in Docker
             build_output = execute_code(output_code, language)
@@ -263,13 +308,12 @@ def main():
             build_output = execute_code(output_code, language)
 
             if build_output != "":
-                message = f"<INST>Here is my current code:\\n
-```\\n{output_code}\\n```Here is the latest execution error when I try to run the code:\\n{build_output}\\n\\n{requirements_prompt}\\n\\n</INST> </s>\\n"
+                message = f"<INST>Here is my current code:\\n```\\n{output_code}\\n```Here is the latest execution error when I try to run the code:\\n{build_output}\\n\\n{requirements_prompt}\\n\\n</INST> </s>\\n"
             else:
                 message = f"<INST>Here is my current code:\\n```\\n{output_code}\\n```\\n\\n{requirements_prompt}\\n\\n</INST> </s>\\n"
 
             # Get response from LLM
-            new_requirements = run_llm(message, "", language)
+            new_requirements = run_llm("", None, message, language)
 
             if new_requirements != "":
                 with open(f"{app_results_folder}/requirements.txt", "w") as f:
